92f02fe73c225835066b021ce2e34f18e651610b
adrhuerta <adrhuerta@gmail.com>
2021-03-11 21:55:12 -0500

    normals/anomalies values

---
+++ process/step_05.R
@@ -0,0 +1,90 @@
+rm(list = ls())
+
+library(xts)
+library(raster)
+"%>%" = magrittr::`%>%`
+
+source('./src/process/Merging/MG_make_single_point.R')
+source('./src/process/Merging/MG_normal_anomaly_values.R')
+
+# data
+qc_data <- readRDS("./data/processed/obs/qc_output/QC_GF_HG_data.RDS")
+
+# grid data
+gridded_data <- raster::brick("/home/adrian/Documents/Repos/PISCOt_v1-2/data/processed/gridded/DEM.nc")[[1]]
+gridded_data_a <- raster::aggregate(gridded_data, 1)
+
+# data to sp
+xyz_sp <- sp::SpatialPointsDataFrame(coords = qc_data$xyz[, c("LON", "LAT")],
+                                     data = qc_data$xyz,
+                                     proj4string = sp::CRS(raster::projection(gridded_data)))
+
+# point that are in the same
+points_to_be_merged <- make_single_point(pts = xyz_sp,
+                                         rgrid = gridded_data_a)
+
+# getting single points 
+new_xyz <- points_to_be_merged$xyz
+
+# points to be merged
+id_to_merge <- points_to_be_merged$no_single_points
+
+# single ts
+new_value_tmax <- qc_data$value$tmax[, -match(unlist(id_to_merge), colnames(qc_data$value$tmax))]
+new_value_tmin <- qc_data$value$tmin[, -match(unlist(id_to_merge), colnames(qc_data$value$tmin))]
+
+# merging no-single ts
+id_to_merge_tmax <- id_to_merge %>%
+  setNames(id_to_merge %>% lapply(function(x) paste(x, collapse = "_"))) %>%
+  lapply(function(x){
+    qc_data$value$tmax[, x] %>% apply(1, mean, na.rm = TRUE) %>% round(2)
+  }) %>%
+  do.call("cbind", .)
+
+id_to_merge_tmax <- xts::xts(id_to_merge_tmax, time(qc_data$value$tmax))
+
+
+id_to_merge_tmin <- id_to_merge %>%
+  setNames(id_to_merge %>% lapply(function(x) paste(x, collapse = "_"))) %>%
+  lapply(function(x){
+    qc_data$value$tmin[, x] %>% apply(1, mean, na.rm = TRUE) %>% round(2)
+  }) %>%
+  do.call("cbind", .)
+
+id_to_merge_tmin <- xts::xts(id_to_merge_tmin, time(qc_data$value$tmin))
+
+# single + new single ts
+new_value_tmax <- cbind(new_value_tmax, id_to_merge_tmax)
+new_value_tmin <- cbind(new_value_tmin, id_to_merge_tmin)
+
+# are ID same?
+all(colnames(new_value_tmax) == new_xyz@data$ID)
+all(colnames(new_value_tmin) == new_xyz@data$ID)
+
+# getting normal/daily anomalies
+
+#tmax
+normal_tmax <- lapply(new_value_tmax, function(x) get_monthly_normals(daily_time_serie = x)) %>%
+  do.call("cbind", .)
+
+anomaly_tmax <- lapply(new_value_tmax, function(x) get_anomaly_values(daily_time_serie = x)) %>%
+  do.call("cbind", .)
+
+#tmin
+normal_tmin <- lapply(new_value_tmin, function(x) get_monthly_normals(daily_time_serie = x)) %>%
+  do.call("cbind", .)
+
+anomaly_tmin <- lapply(new_value_tmin, function(x) get_anomaly_values(daily_time_serie = x)) %>%
+  do.call("cbind", .)
+
+# save data
+
+saveRDS(object = list(values = list(tmax = normal_tmax,
+                                    tmin = normal_tmin),
+                      xyz = new_xyz),
+        file = "./data/processed/obs/qc_output/Normals_OBS.RDS")
+
+saveRDS(object = list(values = list(tmax = anomaly_tmax,
+                                    tmin = anomaly_tmin),
+                      xyz = new_xyz),
+        file = "./data/processed/obs/qc_output/Anomalies_OBS.RDS")
---
+++ src/process/Merging/MG_normal_anomaly_values.R
@@ -0,0 +1,32 @@
+# without seasonal variability?
+get_monthly_normals <- function(daily_time_serie,
+                                as_xts = FALSE)
+{
+
+  mean_values <- sapply(1:12, function(x) round(mean(daily_time_serie[xts::.indexmon(daily_time_serie) %in% (x - 1)], na.rm = TRUE), 2))
+  mean_ts <- daily_time_serie
+  
+  for(i in 1:12){
+    mean_ts[xts::.indexmon(mean_ts) %in% (i - 1)] <- mean_values[i]
+  }
+  
+  if(isTRUE(as_xts)){
+    
+    mean_ts
+    
+  } else {
+    
+    mean_values
+    
+  }
+
+
+}
+
+get_anomaly_values <- function(daily_time_serie)
+{
+  
+  mean_ts <- get_monthly_normals(daily_time_serie = daily_time_serie, as_xts = TRUE)
+  daily_time_serie - mean_ts
+  
+}
