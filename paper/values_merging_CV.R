86839677ca0fb87d7d0c3d425e06780f46762587
adrhuerta <adrhuerta@gmail.com>
2021-03-20 19:17:44 -0500

    ok2

--- paper/values_merging_CV.R
+++ paper/values_merging_CV.R
@@ -59,14 +59,18 @@ for(i in 1:10){
                                                           covs_list = covs_list_tmax,
                                                           obs = qc_data_cv)
                        
-                       tmax_i_gridded <- RK(obs_cov_data = tmax_i, resFitting = 10) + tmax_i$covs$CL
+                       # tmax_i_gridded <- RK(obs_cov_data = tmax_i, resFitting = 10) + tmax_i$covs$CL
+                       tmax_i_gridded2 <- suppressWarnings(
+                         RK(obs_cov_data = tmax_i, resFitting = 10,
+                            extentArea = raster::extent(to_extract_value) + c(-1, 1, -1, 1)) + tmax_i$covs$CL
+                         )
                        
-                       extracted_valued <- raster::extract(tmax_i_gridded, to_extract_value)
+                       extracted_valued <- raster::extract(tmax_i_gridded2, to_extract_value)
                        extracted_valued <- matrix(extracted_valued, nrow = 1, ncol = length(extracted_valued))
                        colnames(extracted_valued) <- assessment(cv_i)$ID
                        extracted_valued
                        
-                     }, mc.cores = 2) -> tmax_cv_i
+                     }, mc.cores = 10) -> tmax_cv_i
   
   saveRDS(object = do.call("cbind", tmax_cv_i) %>% .[, match(stations_CV@data$ID, colnames(.))],
           file = file.path(output_anomalies, sprintf("%s/tmax_%s.RDS", "tmax",  date_i)))
@@ -88,15 +92,18 @@ for(i in 1:10){
                                                           covs_list = covs_list_tmin,
                                                           obs = qc_data_cv)
                        
+                       # tmin_i_gridded <- (RK(obs_cov_data = tmin_i, resFitting = 10) + tmin_i$covs$CL)
+                       tmin_i_gridded2 <- suppressWarnings(
+                         RK(obs_cov_data = tmin_i, resFitting = 10,
+                            extentArea = raster::extent(to_extract_value) + c(-1, 1, -1, 1)) + tmin_i$covs$CL
+                         )
                        
-                       tmin_i_gridded <- (RK(obs_cov_data = tmin_i, resFitting = 10) + tmin_i$covs$CL)
-                       
-                       extracted_valued <- raster::extract(tmin_i_gridded, to_extract_value)
+                       extracted_valued <- raster::extract(tmin_i_gridded2, to_extract_value)
                        extracted_valued <- matrix(extracted_valued, nrow = 1, ncol = length(extracted_valued))
                        colnames(extracted_valued) <- assessment(cv_i)$ID
                        extracted_valued
                        
-                     }, mc.cores = 5) -> tmin_cv_i
+                     }, mc.cores = 10) -> tmin_cv_i
   
   
   saveRDS(object =do.call("cbind", tmin_cv_i) %>% .[, match(stations_CV@data$ID, colnames(.))],
