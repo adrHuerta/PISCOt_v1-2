6c055e660639d6e1ea17d88edaf00cc7761ddfa9
adrhuerta <adrhuerta@gmail.com>
2022-01-28 01:08:11 -0500

    no spcv

--- paper/normal_merging_CV.R
+++ paper/normal_merging_CV.R
@@ -27,75 +27,75 @@ covs_list_tmax <- list(dynamic = list(LST = LST_day),
 covs_list_tmin <- list(dynamic = list(LST = LST_night),
                        static = list(DEM = DEM, X = X, Y = Y))
 
-# kfold validation (spatial)
-stations_CV <- qc_data$xyz
-set.seed(2020+1)
-folds <- spatial_clustering_cv(stations_CV@data, coords = c("LON", "LAT"), v = 10)
-
-for(i in 1:12){
-  
-  parallel::mclapply(folds$splits,
-                     function(cv_i){
-                       
-                       assessment_cv <- assessment(cv_i)$ID
-                       assessment_cv <- match(assessment_cv, qc_data$xyz@data$ID)
-                       to_extract_value  <- qc_data$xyz[assessment_cv, ]
-                       
-                       qc_data_cv <- qc_data
-                       qc_data_cv$values$tmax <- qc_data_cv$values$tmax[,-assessment_cv]
-                       qc_data_cv$xyz <- qc_data_cv$xyz[-assessment_cv,]
-                       
-                       tmax_i <- make_Normal_coVariables(month_value = i,
-                                                         var = "tmax",
-                                                         covs_list = covs_list_tmax,
-                                                         obs = qc_data_cv)
-                       
-                       # tmax_i_gridded <- GWRK(obs_cov_data = tmax_i, resFitting = 10)
-                       tmax_i_gridded2 <- GWRK(obs_cov_data = tmax_i, resFitting = 10,
-                                              extentArea = raster::extent(to_extract_value) + c(-1, 1, -1, 1))
-                       
-                       extracted_valued <- raster::extract(tmax_i_gridded2, to_extract_value)
-                       extracted_valued <- matrix(extracted_valued, nrow = 1, ncol = length(extracted_valued))
-                       colnames(extracted_valued) <- assessment(cv_i)$ID
-                       extracted_valued
-                       
-                     }, mc.cores = 10) -> tmax_cv_i
-  
-  saveRDS(object = do.call("cbind", tmax_cv_i) %>% .[, match(stations_CV@data$ID, colnames(.))],
-          file = file.path(output_normals, sprintf("%s/tmax_spcv_%02d.RDS", "tmax",  i)))
-  
-  
-  parallel::mclapply(folds$splits,
-                     function(cv_i){
-                       
-                       assessment_cv <- assessment(cv_i)$ID
-                       assessment_cv <- match(assessment_cv, qc_data$xyz@data$ID)
-                       to_extract_value  <- qc_data$xyz[assessment_cv, ]
-                       
-                       qc_data_cv <- qc_data
-                       qc_data_cv$values$tmin <- qc_data_cv$values$tmin[,-assessment_cv]
-                       qc_data_cv$xyz <- qc_data_cv$xyz[-assessment_cv,]
-                       
-                       tmin_i <- make_Normal_coVariables(month_value = i,
-                                                         var = "tmin",
-                                                         covs_list = covs_list_tmin,
-                                                         obs = qc_data_cv)
-                       
-                       # tmin_i_gridded <- GWRK(obs_cov_data = tmin_i, resFitting = 10)
-                       tmin_i_gridded2 <- GWRK(obs_cov_data = tmin_i, resFitting = 10,
-                                               extentArea = raster::extent(to_extract_value) + c(-1, 1, -1, 1))
-                       
-                       extracted_valued <- raster::extract(tmin_i_gridded2, to_extract_value)
-                       extracted_valued <- matrix(extracted_valued, nrow = 1, ncol = length(extracted_valued))
-                       colnames(extracted_valued) <- assessment(cv_i)$ID
-                       extracted_valued
-                       
-                     }, mc.cores = 10) -> tmin_cv_i
-  
-  saveRDS(object = do.call("cbind", tmin_cv_i) %>% .[, match(stations_CV@data$ID, colnames(.))],
-          file = file.path(output_normals, sprintf("%s/tmin_spcv_%02d.RDS", "tmin",  i)))
-  
-  }
+# # kfold validation (spatial)
+# stations_CV <- qc_data$xyz
+# set.seed(2020+1)
+# folds <- spatial_clustering_cv(stations_CV@data, coords = c("LON", "LAT"), v = 10)
+# 
+# for(i in 1:12){
+#   
+#   parallel::mclapply(folds$splits,
+#                      function(cv_i){
+#                        
+#                        assessment_cv <- assessment(cv_i)$ID
+#                        assessment_cv <- match(assessment_cv, qc_data$xyz@data$ID)
+#                        to_extract_value  <- qc_data$xyz[assessment_cv, ]
+#                        
+#                        qc_data_cv <- qc_data
+#                        qc_data_cv$values$tmax <- qc_data_cv$values$tmax[,-assessment_cv]
+#                        qc_data_cv$xyz <- qc_data_cv$xyz[-assessment_cv,]
+#                        
+#                        tmax_i <- make_Normal_coVariables(month_value = i,
+#                                                          var = "tmax",
+#                                                          covs_list = covs_list_tmax,
+#                                                          obs = qc_data_cv)
+#                        
+#                        # tmax_i_gridded <- GWRK(obs_cov_data = tmax_i, resFitting = 10)
+#                        tmax_i_gridded2 <- GWRK(obs_cov_data = tmax_i, resFitting = 10,
+#                                               extentArea = raster::extent(to_extract_value) + c(-1, 1, -1, 1))
+#                        
+#                        extracted_valued <- raster::extract(tmax_i_gridded2, to_extract_value)
+#                        extracted_valued <- matrix(extracted_valued, nrow = 1, ncol = length(extracted_valued))
+#                        colnames(extracted_valued) <- assessment(cv_i)$ID
+#                        extracted_valued
+#                        
+#                      }, mc.cores = 10) -> tmax_cv_i
+#   
+#   saveRDS(object = do.call("cbind", tmax_cv_i) %>% .[, match(stations_CV@data$ID, colnames(.))],
+#           file = file.path(output_normals, sprintf("%s/tmax_spcv_%02d.RDS", "tmax",  i)))
+#   
+#   
+#   parallel::mclapply(folds$splits,
+#                      function(cv_i){
+#                        
+#                        assessment_cv <- assessment(cv_i)$ID
+#                        assessment_cv <- match(assessment_cv, qc_data$xyz@data$ID)
+#                        to_extract_value  <- qc_data$xyz[assessment_cv, ]
+#                        
+#                        qc_data_cv <- qc_data
+#                        qc_data_cv$values$tmin <- qc_data_cv$values$tmin[,-assessment_cv]
+#                        qc_data_cv$xyz <- qc_data_cv$xyz[-assessment_cv,]
+#                        
+#                        tmin_i <- make_Normal_coVariables(month_value = i,
+#                                                          var = "tmin",
+#                                                          covs_list = covs_list_tmin,
+#                                                          obs = qc_data_cv)
+#                        
+#                        # tmin_i_gridded <- GWRK(obs_cov_data = tmin_i, resFitting = 10)
+#                        tmin_i_gridded2 <- GWRK(obs_cov_data = tmin_i, resFitting = 10,
+#                                                extentArea = raster::extent(to_extract_value) + c(-1, 1, -1, 1))
+#                        
+#                        extracted_valued <- raster::extract(tmin_i_gridded2, to_extract_value)
+#                        extracted_valued <- matrix(extracted_valued, nrow = 1, ncol = length(extracted_valued))
+#                        colnames(extracted_valued) <- assessment(cv_i)$ID
+#                        extracted_valued
+#                        
+#                      }, mc.cores = 10) -> tmin_cv_i
+#   
+#   saveRDS(object = do.call("cbind", tmin_cv_i) %>% .[, match(stations_CV@data$ID, colnames(.))],
+#           file = file.path(output_normals, sprintf("%s/tmin_spcv_%02d.RDS", "tmin",  i)))
+#   
+#   }
 
 # kfold validation (no spatial)
 set.seed(2020+1)
--- paper/normal_merging_CV_analysis.R
+++ paper/normal_merging_CV_analysis.R
@@ -37,12 +37,15 @@ plt1 <- lattice::xyplot(LAT ~ LON, groups = kfold, data = do.call("rbind", exp) 
 plt2 <- lattice::xyplot(LAT ~ LON, groups = kfold, data = do.call("rbind", exp) %>% subset(nospcv == FALSE),
                 pch  =c(1:10), xlab = "", ylab = "")
 
-c(plt1, plt2) %>%
+# c(plt1, plt2) %>%
+#   update(ylim = c(-18.575, 1.275), xlim = c(-81.325, -67.175), xlab = "", ylab = "") +
+#   latticeExtra::layer(sp::sp.polygons(shp_peru, fill = NA, col = "gray50"), under = TRUE, superpose = FALSE) +
+#   latticeExtra::layer(sp::sp.polygons(shp_sa, fill = NA, col = "gray50"), under = TRUE, superpose = FALSE)
+plt2 %>%
   update(ylim = c(-18.575, 1.275), xlim = c(-81.325, -67.175), xlab = "", ylab = "") +
   latticeExtra::layer(sp::sp.polygons(shp_peru, fill = NA, col = "gray50"), under = TRUE, superpose = FALSE) +
   latticeExtra::layer(sp::sp.polygons(shp_sa, fill = NA, col = "gray50"), under = TRUE, superpose = FALSE)
-
-
+  
 ####### statistics for spcv/nospcv #######
 
 tmax_obs <- qc_data$values$tmax[, stations_CV@data$ID]
@@ -50,11 +53,11 @@ tmin_obs <- qc_data$values$tmin[, stations_CV@data$ID]
 
 # cv 
 output_normals <- "./paper/others/normals"
-tmax_spcv <- file.path(output_normals, sprintf("%s/tmax_spcv_%02d.RDS", "tmax",  1:12)) %>%
-  lapply(function(x) readRDS(x)) %>% do.call("rbind", .)
-
-tmin_spcv <- file.path(output_normals, sprintf("%s/tmin_spcv_%02d.RDS", "tmin",  1:12)) %>%
-  lapply(function(x) readRDS(x)) %>% do.call("rbind", .)
+# tmax_spcv <- file.path(output_normals, sprintf("%s/tmax_spcv_%02d.RDS", "tmax",  1:12)) %>%
+#   lapply(function(x) readRDS(x)) %>% do.call("rbind", .)
+# 
+# tmin_spcv <- file.path(output_normals, sprintf("%s/tmin_spcv_%02d.RDS", "tmin",  1:12)) %>%
+#   lapply(function(x) readRDS(x)) %>% do.call("rbind", .)
 
 tmax_nospcv <- file.path(output_normals, sprintf("%s/tmax_nospcv_%02d.RDS", "tmax",  1:12)) %>%
   lapply(function(x) readRDS(x)) %>% do.call("rbind", .)
@@ -66,50 +69,50 @@ tmin_nospcv <- file.path(output_normals, sprintf("%s/tmin_nospcv_%02d.RDS", "tmi
 cold_months <- c(4, 5, 6, 7, 8, 9)
 warm_months <- c(10, 11, 12, 1, 2, 3)
 
-# spcv
-tmax_spcv_stat_warm <- parallel::mcmapply(function(obs, model){
-  
-  openair::modStats(mod = "model", obs = "obs",
-                    mydata = data.frame(obs = obs, model = model), 
-                    statistic = c("MB", "MGE"))
-}, 
-obs = data.frame(tmax_obs[warm_months, ]),
-model = data.frame(tmax_spcv[warm_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
-  do.call("rbind", .) %>%
-  data.frame(., var = "Tmax", cv = "spcv", season = "Oct.-Mar.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
-
-tmax_spcv_stat_cold <- parallel::mcmapply(function(obs, model){
-  
-  openair::modStats(mod = "model", obs = "obs",
-                    mydata = data.frame(obs = obs, model = model), 
-                    statistic = c("MB", "MGE"))
-}, 
-obs = data.frame(tmax_obs[cold_months, ]),
-model = data.frame(tmax_spcv[cold_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
-  do.call("rbind", .) %>%
-  data.frame(., var = "Tmax", cv = "spcv", season = "Apr.-Sep.")  %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
-
-tmin_spcv_stat_warm <- parallel::mcmapply(function(obs, model){
-  
-  openair::modStats(mod = "model", obs = "obs",
-                    mydata = data.frame(obs = obs, model = model), 
-                    statistic = c("MB", "MGE"))
-}, 
-obs = data.frame(tmin_obs[warm_months, ]),
-model = data.frame(tmin_spcv[warm_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
-  do.call("rbind", .) %>%
-  data.frame(., var = "Tmin", cv = "spcv", season = "Oct.-Mar.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
-
-tmin_spcv_stat_cold <- parallel::mcmapply(function(obs, model){
-  
-  openair::modStats(mod = "model", obs = "obs",
-                    mydata = data.frame(obs = obs, model = model), 
-                    statistic = c("MB", "MGE"))
-}, 
-obs = data.frame(tmin_obs[cold_months, ]),
-model = data.frame(tmin_spcv[cold_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
-  do.call("rbind", .) %>%
-  data.frame(., var = "Tmin", cv = "spcv", season = "Apr.-Sep.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
+# # spcv
+# tmax_spcv_stat_warm <- parallel::mcmapply(function(obs, model){
+#   
+#   openair::modStats(mod = "model", obs = "obs",
+#                     mydata = data.frame(obs = obs, model = model), 
+#                     statistic = c("MB", "MGE"))
+# }, 
+# obs = data.frame(tmax_obs[warm_months, ]),
+# model = data.frame(tmax_spcv[warm_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
+#   do.call("rbind", .) %>%
+#   data.frame(., var = "Tmax", cv = "spcv", season = "Oct.-Mar.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
+# 
+# tmax_spcv_stat_cold <- parallel::mcmapply(function(obs, model){
+#   
+#   openair::modStats(mod = "model", obs = "obs",
+#                     mydata = data.frame(obs = obs, model = model), 
+#                     statistic = c("MB", "MGE"))
+# }, 
+# obs = data.frame(tmax_obs[cold_months, ]),
+# model = data.frame(tmax_spcv[cold_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
+#   do.call("rbind", .) %>%
+#   data.frame(., var = "Tmax", cv = "spcv", season = "Apr.-Sep.")  %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
+# 
+# tmin_spcv_stat_warm <- parallel::mcmapply(function(obs, model){
+#   
+#   openair::modStats(mod = "model", obs = "obs",
+#                     mydata = data.frame(obs = obs, model = model), 
+#                     statistic = c("MB", "MGE"))
+# }, 
+# obs = data.frame(tmin_obs[warm_months, ]),
+# model = data.frame(tmin_spcv[warm_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
+#   do.call("rbind", .) %>%
+#   data.frame(., var = "Tmin", cv = "spcv", season = "Oct.-Mar.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
+# 
+# tmin_spcv_stat_cold <- parallel::mcmapply(function(obs, model){
+#   
+#   openair::modStats(mod = "model", obs = "obs",
+#                     mydata = data.frame(obs = obs, model = model), 
+#                     statistic = c("MB", "MGE"))
+# }, 
+# obs = data.frame(tmin_obs[cold_months, ]),
+# model = data.frame(tmin_spcv[cold_months, ]), SIMPLIFY = FALSE, mc.cores = 5) %>%
+#   do.call("rbind", .) %>%
+#   data.frame(., var = "Tmin", cv = "spcv", season = "Apr.-Sep.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
 
 
 # nospcv
@@ -158,10 +161,11 @@ model = data.frame(tmin_nospcv[cold_months, ]), SIMPLIFY = FALSE, mc.cores = 5) 
   data.frame(., var = "Tmin", cv = "nospcv", season = "Apr.-Sep.") %>% cbind(., stations_CV@data[, c("ID", "LON", "LAT")])
 
 normals_cv <-
-  rbind(tmax_spcv_stat_warm,
-      tmax_spcv_stat_cold,
-      tmin_spcv_stat_warm,
-      tmin_spcv_stat_cold,
+  rbind(
+      # tmax_spcv_stat_warm,
+      # tmax_spcv_stat_cold,
+      # tmin_spcv_stat_warm,
+      # tmin_spcv_stat_cold,
       tmax_nospcv_stat_warm,
       tmax_nospcv_stat_cold,
       tmin_nospcv_stat_warm,
@@ -273,92 +277,92 @@ plt_nospcv_MAE <- normals_cv %>% subset(cv == "nospcv") %>%
     vjust   = -1
   )
 
-plt_spcv_bias <- normals_cv %>% subset(cv == "spcv") %>%
-  ggplot() + 
-  geom_polygon(data = shp_sa %>% broom::tidy(),
-               aes(x = long, y = lat, group = group),
-               fill = NA, colour = "gray20", size = 0.3) +
-  geom_polygon(data = shp_peru %>% broom::tidy(),
-               aes(x = long, y = lat, group = group),
-               fill = NA, colour = "gray20", size = 0.3) + 
-  geom_point(aes(x = LON, y = LAT, color = bias), shape = 19, size = 2) + 
-  facet_grid(season~var, switch = "y") + 
-  #scale_fill_manual(values = cols1) + 
-  scale_color_manual(values = cols1, drop = FALSE) +
-  theme_bw() + 
-  theme(axis.title = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill = NA),
-        strip.text = element_text(size = 12),
-        legend.position="bottom",
-        legend.spacing.x = unit(.1, 'cm'),
-        legend.spacing.y = unit(.05, 'cm'),
-        legend.margin=margin(0,0,0,0),
-        legend.box.margin=margin(-5,-5,-5,-5),
-        legend.text = element_text(
-          margin = margin(l = 4, r = 4, unit = "pt"),
-          hjust = 0)
-  ) +
-  guides(color = guide_legend(override.aes = list(size = 3),
-                              nrow = 1, byrow = TRUE,
-                              label.position = "bottom",
-                              title="bias\n(°C)")) +
-  xlab("") + ylab("") + 
-  coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 1.275), xlim = c(-81.325, -67.175))  + 
-  geom_text(
-    data    = overall_values %>% subset(cv == "spcv") %>% .[, c("season","var", "MB")],
-    mapping = aes(x = -Inf, y = -Inf, label = paste("Overall:", MB, "°C", sep = " ")), size = 4,
-    hjust   = -0.1,
-    vjust   = -1
-  )
-
-  
-plt_spcv_MAE <- normals_cv %>% subset(cv == "spcv") %>%
-  ggplot() + 
-  geom_polygon(data = shp_sa %>% broom::tidy(),
-               aes(x = long, y = lat, group = group),
-               fill = NA, colour = "gray20", size = 0.3) +
-  geom_polygon(data = shp_peru %>% broom::tidy(),
-               aes(x = long, y = lat, group = group),
-               fill = NA, colour = "gray20", size = 0.3) + 
-  geom_point(aes(x = LON, y = LAT, color = MAE), shape = 19, size = 2) + 
-  facet_grid(season~var, switch = "y") + 
-  #scale_fill_manual(values = cols2) + 
-  scale_color_manual(values = cols2, drop = FALSE) +
-  theme_bw() + 
-  theme(axis.title = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill = NA),
-        strip.text = element_text(size = 12),
-        legend.position="bottom",
-        legend.spacing.x = unit(.1, 'cm'),
-        legend.spacing.y = unit(.05, 'cm'),
-        legend.margin=margin(0,0,0,0),
-        legend.box.margin=margin(-5,-5,-5,-5),
-        legend.text = element_text(
-          margin = margin(l = 4, r = 4, unit = "pt"),
-          hjust = 0)
-  ) +
-  guides(color = guide_legend(override.aes = list(size = 3),
-                              nrow = 1, byrow = TRUE,
-                              label.position = "bottom",
-                              title="MAE\n(°C)")) +
-  xlab("") + ylab("") + 
-  coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 1.275), xlim = c(-81.325, -67.175))   + 
-  geom_text(
-    data    = overall_values %>% subset(cv == "spcv") %>% .[, c("season","var", "MGE")],
-    mapping = aes(x = -Inf, y = -Inf, label = paste("Overall:", MGE, "°C", sep = " ")), size = 3.5,
-    hjust   = -0.1,
-    vjust   = -1
-  )
+# plt_spcv_bias <- normals_cv %>% subset(cv == "spcv") %>%
+#   ggplot() + 
+#   geom_polygon(data = shp_sa %>% broom::tidy(),
+#                aes(x = long, y = lat, group = group),
+#                fill = NA, colour = "gray20", size = 0.3) +
+#   geom_polygon(data = shp_peru %>% broom::tidy(),
+#                aes(x = long, y = lat, group = group),
+#                fill = NA, colour = "gray20", size = 0.3) + 
+#   geom_point(aes(x = LON, y = LAT, color = bias), shape = 19, size = 2) + 
+#   facet_grid(season~var, switch = "y") + 
+#   #scale_fill_manual(values = cols1) + 
+#   scale_color_manual(values = cols1, drop = FALSE) +
+#   theme_bw() + 
+#   theme(axis.title = element_blank(),
+#         axis.text = element_blank(),
+#         axis.ticks = element_blank(),
+#         panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         strip.background = element_blank(),
+#         panel.border = element_rect(colour = "black", fill = NA),
+#         strip.text = element_text(size = 12),
+#         legend.position="bottom",
+#         legend.spacing.x = unit(.1, 'cm'),
+#         legend.spacing.y = unit(.05, 'cm'),
+#         legend.margin=margin(0,0,0,0),
+#         legend.box.margin=margin(-5,-5,-5,-5),
+#         legend.text = element_text(
+#           margin = margin(l = 4, r = 4, unit = "pt"),
+#           hjust = 0)
+#   ) +
+#   guides(color = guide_legend(override.aes = list(size = 3),
+#                               nrow = 1, byrow = TRUE,
+#                               label.position = "bottom",
+#                               title="bias\n(°C)")) +
+#   xlab("") + ylab("") + 
+#   coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 1.275), xlim = c(-81.325, -67.175))  + 
+#   geom_text(
+#     data    = overall_values %>% subset(cv == "spcv") %>% .[, c("season","var", "MB")],
+#     mapping = aes(x = -Inf, y = -Inf, label = paste("Overall:", MB, "°C", sep = " ")), size = 4,
+#     hjust   = -0.1,
+#     vjust   = -1
+#   )
+# 
+#   
+# plt_spcv_MAE <- normals_cv %>% subset(cv == "spcv") %>%
+#   ggplot() + 
+#   geom_polygon(data = shp_sa %>% broom::tidy(),
+#                aes(x = long, y = lat, group = group),
+#                fill = NA, colour = "gray20", size = 0.3) +
+#   geom_polygon(data = shp_peru %>% broom::tidy(),
+#                aes(x = long, y = lat, group = group),
+#                fill = NA, colour = "gray20", size = 0.3) + 
+#   geom_point(aes(x = LON, y = LAT, color = MAE), shape = 19, size = 2) + 
+#   facet_grid(season~var, switch = "y") + 
+#   #scale_fill_manual(values = cols2) + 
+#   scale_color_manual(values = cols2, drop = FALSE) +
+#   theme_bw() + 
+#   theme(axis.title = element_blank(),
+#         axis.text = element_blank(),
+#         axis.ticks = element_blank(),
+#         panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         strip.background = element_blank(),
+#         panel.border = element_rect(colour = "black", fill = NA),
+#         strip.text = element_text(size = 12),
+#         legend.position="bottom",
+#         legend.spacing.x = unit(.1, 'cm'),
+#         legend.spacing.y = unit(.05, 'cm'),
+#         legend.margin=margin(0,0,0,0),
+#         legend.box.margin=margin(-5,-5,-5,-5),
+#         legend.text = element_text(
+#           margin = margin(l = 4, r = 4, unit = "pt"),
+#           hjust = 0)
+#   ) +
+#   guides(color = guide_legend(override.aes = list(size = 3),
+#                               nrow = 1, byrow = TRUE,
+#                               label.position = "bottom",
+#                               title="MAE\n(°C)")) +
+#   xlab("") + ylab("") + 
+#   coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 1.275), xlim = c(-81.325, -67.175))   + 
+#   geom_text(
+#     data    = overall_values %>% subset(cv == "spcv") %>% .[, c("season","var", "MGE")],
+#     mapping = aes(x = -Inf, y = -Inf, label = paste("Overall:", MGE, "°C", sep = " ")), size = 3.5,
+#     hjust   = -0.1,
+#     vjust   = -1
+#   )
 
 library(patchwork)
 (plt_nospcv_bias +
@@ -370,11 +374,11 @@ ggsave(file.path(".", "paper", "output", "Fig_normal_nospcv.jpg"),
        dpi = 250, scale = 1,
        width = 9.5, height = 7, units = "in")
 
-(plt_spcv_bias +
-    theme(plot.margin=margin(l=-0.8,unit="cm")))  +  
-  (plt_spcv_MAE + theme(strip.background.y = element_blank(), strip.text.y = element_blank()) + 
-     theme(plot.margin=margin(l=-0.8,unit="cm")))
-
-ggsave(file.path(".", "paper", "output", "Fig_normal_spcv.jpg"),
-       dpi = 250, scale = 1,
-       width = 9.5, height = 7, units = "in")
+# (plt_spcv_bias +
+#     theme(plot.margin=margin(l=-0.8,unit="cm")))  +  
+#   (plt_spcv_MAE + theme(strip.background.y = element_blank(), strip.text.y = element_blank()) + 
+#      theme(plot.margin=margin(l=-0.8,unit="cm")))
+# 
+# ggsave(file.path(".", "paper", "output", "Fig_normal_spcv.jpg"),
+#        dpi = 250, scale = 1,
+#        width = 9.5, height = 7, units = "in")
