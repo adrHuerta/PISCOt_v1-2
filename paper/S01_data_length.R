5ed8e85144399269ad995bfac06555b850119505
adrhuerta <adrhuerta@gmail.com>
2022-11-03 22:58:49 -0500

    lattice plot -> ggplot

--- paper/S01_data_length.R
+++ paper/S01_data_length.R
@@ -39,25 +39,69 @@ tn <- xts(tn, time(tn_data))
 tn <- tn["/2020"]
 
 #
-tx_qc <- apply(qc_data$values$tmax, 1, function(x) sum(!is.na(x)))
-tx_qc <- xts(tx_qc, time(qc_data$values$tmax))
-tx_qc <- tx_qc["/2020"]
+# tx_qc <- apply(qc_data$values$tmax, 1, function(x) sum(!is.na(x)))
+# tx_qc <- xts(tx_qc, time(qc_data$values$tmax))
+# tx_qc <- tx_qc["/2020"]
+# 
+# tn_qc <- apply(qc_data$values$tmin, 1, function(x) sum(!is.na(x)))
+# tn_qc <- xts(tn_qc, time(qc_data$values$tmin))
+# tn_qc <- tn_qc["/2020"]
+# 
+# 
+# xyplot(tx, par.settings = mytheme, col = "tomato", lwd = 2) + 
+# xyplot(tn, par.settings = mytheme, col = "royalblue", lwd = 2) -> p1
+# 
+# xyplot(tx_qc, par.settings = mytheme, col = "tomato", lwd = 2) + 
+#   xyplot(tn_qc, par.settings = mytheme, col = "royalblue", lwd = 2) -> p2
+# 
+# c("Raw" = p1, "After QC" = p2, layout = c(1, 2)) %>%
+#   update(ylim = c(0, 450), ylab = "Number of observations", xlab = "") -> pfinal
+# 
+# pdf(file = file.path(".", "paper", "output", "Figure_S01_data_lenght.pdf"),
+#     width = 6, height = 5)
+# print(pfinal)
+# dev.off()
 
-tn_qc <- apply(qc_data$values$tmin, 1, function(x) sum(!is.na(x)))
-tn_qc <- xts(tn_qc, time(qc_data$values$tmin))
-tn_qc <- tn_qc["/2020"]
+library(ggplot2)
+
+raw_ts <- cbind(tx, tn) %>% setNames(c("Tmax", "Tmin"))
+raw_ts <- fortify.zoo(raw_ts)
+raw_ts <- reshape2::melt(raw_ts, id.vars = "Index")
+ggplot(raw_ts, aes(x=Index, y=value, colour = variable)) +
+  scale_colour_manual("", values = c("tomato", "royalblue")) +
+  geom_line(size = .25) + theme_bw() + xlab("") + ylab("") + theme(legend.position = c(0.15, 0.5),
+                                                                   legend.justification = "left", 
+                                                                   legend.margin = margin(0, 0, 0, 0),
+                                                                   legend.spacing.x = unit(0, "pt"),
+                                                                   legend.box.background = element_rect(colour = "gray50", size = .5),
+                                                                   legend.title = element_blank()) +
+  annotate("text", x = as.Date("1930-01-01"), y = 380, label = "Raw") -> p1
 
 
-xyplot(tx, par.settings = mytheme, col = "tomato", lwd = 2) + 
-xyplot(tn, par.settings = mytheme, col = "royalblue", lwd = 2) -> p1
+qc_ts <- cbind(tx_qc, tn_qc) %>% setNames(c("Tmax", "Tmin"))
+qc_ts <- fortify.zoo(qc_ts) 
+qc_ts <- reshape2::melt(qc_ts, id.vars = "Index")
+ggplot(qc_ts, aes(x=Index, y=value, colour = variable)) +
+  scale_colour_manual(values = c("tomato", "royalblue")) +
+  geom_line(size = .25) + guides(colour="none") +  theme_bw() + xlab("") + ylab("") +
+  annotate("text", x = as.Date("1984-01-01"), y = 345, label = "After QC") -> p2
 
-xyplot(tx_qc, par.settings = mytheme, col = "tomato", lwd = 2) + 
-  xyplot(tn_qc, par.settings = mytheme, col = "royalblue", lwd = 2) -> p2
+library(cowplot)
+library(grid)
+library(gridExtra)
 
-c("Raw" = p1, "After QC" = p2, layout = c(1, 2)) %>%
-  update(ylim = c(0, 450), ylab = "Number of data", xlab = "") -> pfinal
+plot_grid(p1 + theme(plot.margin=grid::unit(c(2,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5)),
+          p2 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5)),
+          ncol = 1, nrow = 2) -> plot_ps
 
-pdf(file = file.path(".", "paper", "output", "Figure_S01_data_lenght.pdf"),
-     width = 6, height = 5)
-print(pfinal)
-dev.off()
+y.grob <- textGrob("Number of observations", 
+                   gp=gpar(col="black", fontsize=12), rot=90)
+
+plot_ps <- grid.arrange(arrangeGrob(plot_ps, left = y.grob))
+
+ggsave(file.path(".", "paper", "output", "Figure_S01_data_lenght.pdf"),
+       device = "pdf",
+       plot = plot_ps,
+       dpi = 500, scale = 0.75,
+       width = 6, height = 5, units = "in")
+
--- paper/S03_non_qc_data_length.R
+++ paper/S03_non_qc_data_length.R
@@ -85,20 +85,100 @@ QC5_ts_tmin <- xts::merge.xts(QC5_ts_tmin, to_fill); QC5_ts_tmin <- QC5_ts_tmin[
 QC6_ts_tmax <- xts::merge.xts(QC6_ts_tmax, to_fill); QC6_ts_tmax <- QC6_ts_tmax["/2020"]
 QC6_ts_tmin <- xts::merge.xts(QC6_ts_tmin, to_fill); QC6_ts_tmin <- QC6_ts_tmin["/2020"]
 
-xyplot(QC1_ts, cex = .5, type = "p", col = "black", auto.key = FALSE) -> p1
-xyplot(cbind(QC2_ts_tmax, QC2_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p2
-xyplot(QC3_ts, cex = .5, type = "p", col = "black", auto.key = FALSE) -> p3
-xyplot(cbind(QC4_ts_tmax, QC4_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p4
-xyplot(cbind(QC5_ts_tmax, QC5_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p5
-xyplot(cbind(QC6_ts_tmax, QC6_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p6
+library(ggplot2)
 
-c("(i) Obvious errors" = p1, "(ii) Extreme values" = p2, 
-  "(iii) Internal consistency" = p3, "(iv) Temporal coherence"= p4, 
-  "(v) Spatial coherence" = p5, "(vi) Visual Inspection" = p6, layout = c(2,3)) %>%
-  update(xlab = "", ylab = "Number of deleted data",
-         par.settings = mytheme) -> pfinal
+QC1_ts <- fortify.zoo(QC1_ts)
+QC1_ts[is.na(QC1_ts)] <- 0
+ggplot(QC1_ts, aes(x=Index, y=QC1_ts), fill = "black") + 
+  geom_bar(stat="identity", na.rm = TRUE) + theme_bw() + xlab("") + ylab("") +
+  annotate("text", x = as.Date("1940-01-01"), y = 40, label = "i) Obvious errors") -> p1
+  
 
-pdf(file = file.path(".", "paper", "output", "Figure_S03_non_qc_data_lenght.pdf"),
-     width = 8, height = 5)
-print(pfinal)
-dev.off()
+QC2_ts <- cbind(QC2_ts_tmax, QC2_ts_tmin)
+QC2_ts[is.na(QC2_ts)] <- 0
+QC2_ts <- fortify.zoo(QC2_ts)
+QC2_ts <- reshape2::melt(QC2_ts, id.vars = "Index")
+ggplot(QC2_ts, aes(x=Index, y=value, fill = variable)) +
+  scale_fill_manual(values = c("tomato", "royalblue")) +
+  geom_bar(stat="identity") + theme_bw() + guides(fill="none") + xlab("") + ylab("") +
+  annotate("text", x = as.Date("1940-01-01"), y = 30, label = "ii) Extreme values") -> p2
+
+
+
+QC3_ts <- fortify.zoo(QC3_ts)
+QC3_ts[is.na(QC3_ts)] <- 0
+ggplot(QC3_ts, aes(x=Index, y=QC3_ts), fill = "black") + 
+  geom_bar(stat="identity", na.rm = TRUE) + theme_bw() + xlab("") + ylab("") +
+  scale_y_continuous(labels = c(0, 1), breaks = c(0, 1)) +
+  annotate("text", x = as.Date("1945-01-01"), y = .9, label = "iii) Internal consistency values") -> p3
+
+
+QC4_ts <- cbind(QC4_ts_tmax, QC4_ts_tmin)
+QC4_ts[is.na(QC4_ts)] <- 0
+QC4_ts <- fortify.zoo(QC4_ts)
+QC4_ts <- reshape2::melt(QC4_ts, id.vars = "Index")
+ggplot(QC4_ts, aes(x=Index, y=value, fill = variable)) + 
+  scale_fill_manual(values = c("tomato", "royalblue")) +
+  geom_bar(stat="identity") + theme_bw() + guides(fill="none") + xlab("") + ylab("") +
+  annotate("text", x = as.Date("1942-01-01"), y = 2.75, label = "iv) Temporal coherence") -> p4
+
+
+QC5_ts <- cbind(QC5_ts_tmax, QC5_ts_tmin)
+QC5_ts[is.na(QC5_ts)] <- 0
+QC5_ts <- fortify.zoo(QC5_ts)
+QC5_ts <- reshape2::melt(QC5_ts, id.vars = "Index")
+ggplot(QC5_ts, aes(x=Index, y=value, fill = variable)) + 
+  scale_fill_manual(values = c("tomato", "royalblue")) +
+  geom_bar(stat="identity") + theme_bw() + guides(fill="none") + xlab("") + ylab("") +
+  annotate("text", x = as.Date("1941-01-01"), y = 7.75, label = "v) Spatial coherence") -> p5
+
+
+QC6_ts <- cbind(QC6_ts_tmax, QC6_ts_tmin)
+QC6_ts[is.na(QC6_ts)] <- 0
+QC6_ts <- fortify.zoo(QC6_ts)
+QC6_ts <- reshape2::melt(QC6_ts, id.vars = "Index")
+ggplot(QC6_ts, aes(x=Index, y=value, fill = variable)) + 
+  scale_fill_manual(values = c("tomato", "royalblue")) +
+  geom_bar(stat="identity") + theme_bw() + guides(fill="none") + xlab("") + ylab("") +
+  annotate("text", x = as.Date("1941-01-01"), y = 95, label = "vi) Visual Inspection") -> p6
+
+library(cowplot)
+library(grid)
+library(gridExtra)
+
+plot_grid(p1 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5), axis.text.x = element_blank(), axis.ticks.x=element_blank()),
+          p2 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5), axis.text.x = element_blank(), axis.ticks.x=element_blank()), 
+          p3 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5), axis.text.x = element_blank(), axis.ticks.x=element_blank()), 
+          p4 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5), axis.text.x = element_blank(), axis.ticks.x=element_blank()), 
+          p5 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5), axis.text.x = element_blank(), axis.ticks.x=element_blank()), 
+          p6 + theme(plot.margin=grid::unit(c(0,0,0,0), "mm"), axis.text.y = element_text(angle = 90, vjust = 1, hjust=0.5)),
+          ncol = 1) -> plot_ps
+
+y.grob <- textGrob("Number of deleted observations", 
+                   gp=gpar(col="black", fontsize=12), rot=90)
+
+plot_ps <- grid.arrange(arrangeGrob(plot_ps, left = y.grob))
+
+ggsave(file.path(".", "paper", "output", "Figure_S03_non_qc_data_lenght.pdf"),
+       device = "pdf",
+       plot = plot_ps,
+       dpi = 500, scale = 0.75,
+       width = 8, height = 10, units = "in")
+
+# xyplot(QC1_ts, cex = .5, type = "p", col = "black", auto.key = FALSE) -> p1
+# xyplot(cbind(QC2_ts_tmax, QC2_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p2
+# xyplot(QC3_ts, cex = .5, type = "p", col = "black", auto.key = FALSE) -> p3
+# xyplot(cbind(QC4_ts_tmax, QC4_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p4
+# xyplot(cbind(QC5_ts_tmax, QC5_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p5
+# xyplot(cbind(QC6_ts_tmax, QC6_ts_tmin), superpose = TRUE, type = "p", cex = c(.5, .15), col = c("tomato", "royalblue"), auto.key = FALSE) -> p6
+# 
+# c("(i) Obvious errors" = p1, "(ii) Extreme values" = p2, 
+#   "(iii) Internal consistency" = p3, "(iv) Temporal coherence"= p4, 
+#   "(v) Spatial coherence" = p5, "(vi) Visual Inspection" = p6, layout = c(2,3)) %>%
+#   update(xlab = "", ylab = "Number of deleted observations",
+#          par.settings = mytheme) -> pfinal
+# 
+# pdf(file = file.path(".", "paper", "output", "Figure_S03_non_qc_data_lenght.pdf"),
+#      width = 8, height = 5)
+# print(pfinal)
+# dev.off()
